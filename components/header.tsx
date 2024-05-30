bdad6abab10561f411403e344e7b9d62c8209f45
Bumpist <104641096+parksubeom@users.noreply.github.com>
2024-05-30 14:36:57 +0900

    fix : 버셀 배포에러 수정

--- components/header.tsx
+++ components/header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
     <header className="flex w-screen h-16 bg-cover bg-center bg-[url('/background-main.png')] opacity-95 justify-center items-center fixed z-[1]">
       <div className="flex rel_width item-center justify-between text-3xl">
         <Link href="/">
-          <h1>BUM's PORTFOLIO</h1>
+          <h1>BUM PORTFOLIO</h1>
         </Link>
         <nav className="flex text-lg w-1/2 justify-between">
           <Link href="/">HOME</Link>
