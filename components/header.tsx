a636f230cd1ec741637167feea340a4d87d16060
Bumpist <104641096+parksubeom@users.noreply.github.com>
2024-05-31 17:02:45 +0900

    feat : 헤더 모바일 사이즈 온클릭함수 변경

--- components/header.tsx
+++ components/header.tsx
@@ -31,10 +31,18 @@ export default function Header() {
         <header className="animate-slideDown lg:hidden ${openGnb ? flex : hidden} flex flex-col w-screen h-48 max-h-32 min-h-16 bg-cover bg-center bg-[url('/background-main.png')] opacity-99 justify-center items-center fixed top-[4rem] z-[1] an">
           <div className="flex rel_width item-center justify-start text-3xl ">
             <nav className="flex flex-col text-sm w-1/2 justify-between items-start gap-2">
-              <Link href="/">HOME</Link>
-              <Link href="/about_me">ABOUT ME</Link>
-              <Link href="/project">PROJECT</Link>
-              <Link href="/">CARRER</Link>
+              <Link onClick={() => setOpenGnb(!openGnb)} href="/">
+                HOME
+              </Link>
+              <Link onClick={() => setOpenGnb(!openGnb)} href="/about_me">
+                ABOUT ME
+              </Link>
+              <Link onClick={() => setOpenGnb(!openGnb)} href="/project">
+                PROJECT
+              </Link>
+              <Link onClick={() => setOpenGnb(!openGnb)} href="/">
+                CARRER
+              </Link>
             </nav>
           </div>
         </header>
