c754374cad48ff92d5b99f0000d4606e76adde2f
Bumpist <104641096+parksubeom@users.noreply.github.com>
2024-05-31 16:58:28 +0900

    feat : gnb바 애니메이션 설정

--- components/header.tsx
+++ components/header.tsx
@@ -30,7 +30,7 @@ export default function Header() {
       {openGnb ? (
         <header className="animate-slideDown lg:hidden ${openGnb ? flex : hidden} flex flex-col w-screen h-48 max-h-32 min-h-16 bg-cover bg-center bg-[url('/background-main.png')] opacity-99 justify-center items-center fixed top-[4rem] z-[1] an">
           <div className="flex rel_width item-center justify-start text-3xl ">
-            <nav className="flex flex-col xl:text-lg text-base w-1/2 justify-between items-start gap-2">
+            <nav className="flex flex-col xl:text-lg text-sm w-1/2 justify-between items-start gap-2">
               <Link href="/">HOME</Link>
               <Link href="/about_me">ABOUT ME</Link>
               <Link href="/project">PROJECT</Link>
