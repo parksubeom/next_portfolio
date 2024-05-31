e1dfde0187e399457459f7c53597b781d084192a
Bumpist <104641096+parksubeom@users.noreply.github.com>
2024-05-31 22:43:12 +0900

    feat : 헤더 모바일 사이즈 온클릭함수

--- components/header.tsx
+++ components/header.tsx
@@ -9,13 +9,23 @@ export default function Header() {
     <header className="flex flex-col w-screen h-16 max-h-32 min-h-16 bg-cover bg-center bg-[url('/background-main.png')] opacity-95 justify-center items-center fixed z-[1]">
       <div className="flex rel_width item-center justify-between text-3xl ">
         <Link href="/">
-          <h1 className=" xl:text-2xl text-xl">BUM PORTFOLIO</h1>
+          <h1 className=" xl:text-2xl text-xl hover:text-[--color-sub-002]">
+            BUM PORTFOLIO
+          </h1>
         </Link>
         <nav className="lg:flex hidden xl:text-lg text-base w-1/2 justify-between items-center">
-          <Link href="/">HOME</Link>
-          <Link href="/about_me">ABOUT ME</Link>
-          <Link href="/project">PROJECT</Link>
-          <Link href="/">CARRER</Link>
+          <Link className="hover:text-[--color-sub-002]" href="/">
+            HOME
+          </Link>
+          <Link className="hover:text-[--color-sub-002]" href="/about_me">
+            ABOUT ME
+          </Link>
+          <Link className="hover:text-[--color-sub-002]" href="/project">
+            PROJECT
+          </Link>
+          <Link className="hover:text-[--color-sub-002]" href="/">
+            CARRER
+          </Link>
         </nav>
         <nav className="lg:hidden flex xl:text-lg text-base w-1/2 justify-end items-center">
           <button
@@ -31,16 +41,32 @@ export default function Header() {
         <header className="animate-slideDown lg:hidden ${openGnb ? flex : hidden} flex flex-col w-screen h-48 max-h-32 min-h-16 bg-cover bg-center bg-[url('/background-main.png')] opacity-99 justify-center items-center fixed top-[4rem] z-[1] an">
           <div className="flex rel_width item-center justify-start text-3xl ">
             <nav className="flex flex-col text-sm w-1/2 justify-between items-start gap-2">
-              <Link onClick={() => setOpenGnb(!openGnb)} href="/">
+              <Link
+                className="hover:text-[--color-sub-002]"
+                onClick={() => setOpenGnb(!openGnb)}
+                href="/"
+              >
                 HOME
               </Link>
-              <Link onClick={() => setOpenGnb(!openGnb)} href="/about_me">
+              <Link
+                className="hover:text-[--color-sub-002]"
+                onClick={() => setOpenGnb(!openGnb)}
+                href="/about_me"
+              >
                 ABOUT ME
               </Link>
-              <Link onClick={() => setOpenGnb(!openGnb)} href="/project">
+              <Link
+                className="hover:text-[--color-sub-002]"
+                onClick={() => setOpenGnb(!openGnb)}
+                href="/project"
+              >
                 PROJECT
               </Link>
-              <Link onClick={() => setOpenGnb(!openGnb)} href="/">
+              <Link
+                className="hover:text-[--color-sub-002]"
+                onClick={() => setOpenGnb(!openGnb)}
+                href="/"
+              >
                 CARRER
               </Link>
             </nav>
